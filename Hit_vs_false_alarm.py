103608eff956ee70301e4257fc50dea55c93c110
isagarnreiter <51999763+isagarnreiter@users.noreply.github.com>
2021-02-14 20:15:14 +0000

    Create Hit_vs_false_alarm.py

---
+++ Hit_vs_false_alarm.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Feb 14 17:26:57 2021
+
+@author: Isabelle
+"""
+import scipy.io as sc
+
+data = sc.loadmat('dataDescription')
+
+mouse_data = sc.loadmat('Data_Mouse_PL225_sessionNum_5')

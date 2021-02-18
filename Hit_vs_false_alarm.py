98a3f416125d6084a3d5403e39f8a16e538d7088
isagarnreiter <51999763+isagarnreiter@users.noreply.github.com>
2021-02-18 17:12:40 +0000

    make dataframe

--- Hit_vs_false_alarm.py
+++ Hit_vs_false_alarm.py
@@ -5,7 +5,22 @@ Created on Sun Feb 14 17:26:57 2021
 @author: Isabelle
 """
 import scipy.io as sc
+import pandas as pd
+import os.path
 
-data = sc.loadmat('dataDescription')
+# data = sc.loadmat('dataDescription')
 
-mouse_data = sc.loadmat('Data_Mouse_PL225_sessionNum_5')
+data = {}
+
+for i in range(200, 215):
+    for j in range(1,13):
+        if os.path.isfile(f"./Data_Mouse_PL{i}_sessionNum_{j}.mat") == True:
+            mouse_trial = sc.loadmat(f"Data_Mouse_PL{i}_sessionNum_{j}")
+            mouse_trial = pd.DataFrame(mouse_trial['tmp'][0])
+            data[f'{i}_{j}'] = mouse_trial
+        else:
+            break
+
+
+dataf = pd.concat(data)
+

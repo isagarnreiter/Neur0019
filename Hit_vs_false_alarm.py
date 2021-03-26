2c2a1b59b29f9fc99e79ba487221bedd4749b839
isagarnreiter <51999763+isagarnreiter@users.noreply.github.com>
2021-03-26 16:57:37 +0000

    upd

--- Hit_vs_false_alarm.py
+++ Hit_vs_false_alarm.py
@@ -29,42 +29,54 @@ def decompress_pickle(file):
 #load the data from individual matlab files and save them in a dataframe
 #each matlab file is named Data_Mouse_PL{i}_sessionNum_{j} with i being the 
 #name of the mouse and j being the trial number
-description = sc.loadmat("D:/UserFolder/neur0019/dataDescription.mat")
+
+#description = sc.loadmat("D:/UserFolder/neur0019/dataDescription.mat")
 
 data = {} #create empty dictionary to host data 
 
-for i in [200, 201, 202, 203, 204, 205, 206, 207, 209]:
+for i in [200, 201, 202, 203, 204, 205, 206, 207, 209, 222, 223, 224, 225]:
+    print(f'Mouse {i}')
     for j in range(1,14):
         if os.path.isfile(f"D:/UserFolder/neur0019/Data_Mouse_PL{i}_sessionNum_{j}.mat") == True:
             mouse_trial = sc.loadmat(f"D:/UserFolder/neur0019/Data_Mouse_PL{i}_sessionNum_{j}")
             mouse_trial = pd.DataFrame(mouse_trial['tmp'][0])
-            mouse_trial = pd.DataFrame(mouse_trial.loc[0][['Session_Type','Trial_Counter','Trial_StartTime','Trial_ID', 'Trial_LFP_wS1', 'Trial_LFP_wS2', 'Trial_FirstLickTime']])
+            mouse_trial = pd.DataFrame(mouse_trial.loc[0][['Trial_Counter', 'Trial_ID', 'Trial_LFP_wS1', 'Trial_LFP_wS2', 'Trial_LFP_wM1', 'Trial_LFP_dCA1', 'Trial_LFP_mPFC', 'Trial_FirstLickTime']])
             data[f'{i}_{j}'] = mouse_trial
         else:
             break
 
-DT_sessions = []
-X_sessions = []
-wS1_sessions = []
-for i in range(219,226):
-    mouse_trial_2 = sc.loadmat(f"Data_Mouse_PL{i}_sessionNum_1")
-    mouse_trial_2 = pd.DataFrame(mouse_trial_2['tmp'][0])
-    mouse_trial_2 = pd.DataFrame(mouse_trial_2.loc[0]['Trial_LFP_wS1'][0])
-    
-    if max(mouse_trial_2[0]) != np.nan:
-        wS1_sessions.append(i)        
+#DT_sessions = []
+#X_sessions = []
+#wS1_sessions = []
+#for i in range(219,226):
+#    mouse_trial_2 = sc.loadmat(f"Data_Mouse_PL{i}_sessionNum_1")
+#    mouse_trial_2 = pd.DataFrame(mouse_trial_2['tmp'][0])
+#    mouse_trial_2 = pd.DataFrame(mouse_trial_2.loc[0]['Trial_LFP_wS1'][0])
+#    
+#    if max(mouse_trial_2[0]) != np.nan:
+#        wS1_sessions.append(i)        
+
+for i in mice_nbs:
+    for j in range(1,14):
+        if f'{i}_{j}' in data:
+            for k in [2,3,4,5,6]:
+                data[f'{i}_{j}'][0][k] = data[f'{i}_{j}'][0][k][:, 5800:10000]
+        else:
+            break
 
 dataf = pd.concat(data)
 
-compressed_pickle('D:/UserFolder/neur0019/almost_all_data', almost_all_data) 
+index = ['Trial_LFP_wS1', 'Trial_LFP_wS2', 'Trial_LFP_wM1', 'Trial_LFP_dCA1', 'Trial_LFP_mPFC']
+
+compressed_pickle('D:/UserFolder/neur0019/all_data_short', dataf) 
 
 #dataf.to_pickle('data_concat')
 #diusc = pd.read_pickle('data_concat')
 
 #%%
-almost_all_data = decompress_pickle('D:/UserFolder/neur0019/almost_all_data.pbz2') 
+all_data = decompress_pickle('D:/UserFolder/neur0019/all_data_short.pbz2') 
 
-mice_nbs = [200, 201, 202, 204, 205, 207, 209]    
+mice_nbs = [200, 201, 202, 203, 204, 205, 207, 209, 222, 223, 224, 225]    
 
 
 #%% =============================================================================
@@ -72,32 +84,80 @@ mice_nbs = [200, 201, 202, 204, 205, 207, 209]
 # wS1_stats = dictionary with the average response of a neuron for each mouse depending on the trial condition
 # =============================================================================
 
-wS1 = {}
-for i in mice_nbs:
-    wS1[f'{i}'] = [[], [], [], []]
-    for j in range(1,11):
-        try:
-            for k in range(len(almost_all_data.loc[f'{i}_{j}'][0]['Trial_Counter'])):
-                if almost_all_data.loc[f'{i}_{j}'][0]['Trial_ID'][k][0] == 1:
-                    wS1[f'{i}'][1].append(np.array(almost_all_data.loc[f'{i}_{j}'][0]['Trial_LFP_wS1'][k]))
-                elif almost_all_data.loc[f'{i}_{j}'][0]['Trial_ID'][k][0] == 3:
-                    wS1[f'{i}'][2].append(np.array(almost_all_data.loc[f'{i}_{j}'][0]['Trial_LFP_wS1'][k]))
-                elif almost_all_data.loc[f'{i}_{j}'][0]['Trial_ID'][k][0] == 0:
-                    wS1[f'{i}'][0].append(np.array(almost_all_data.loc[f'{i}_{j}'][0]['Trial_LFP_wS1'][k]))
-                elif almost_all_data.loc[f'{i}_{j}'][0]['Trial_ID'][k][0] == 2:
-                    wS1[f'{i}'][3].append(np.array(almost_all_data.loc[f'{i}_{j}'][0]['Trial_LFP_wS1'][k]))
-        except KeyError:
-            break
+regions = {'wS1': {}, 'wS2': {}, 'wM1': {}, 'dCA1': {}, 'mPFC': {}}
+region_list = list(regions.keys())
 
+LickTime_data = []
+for i in mice_nbs:
+    print(f'Mouse {i}')
+    regions['wS1'][f'{i}'] = [[], [], [], []]
+    regions['wS2'][f'{i}'] = [[], [], [], []]
+    regions['wM1'][f'{i}'] = [[], [], [], []]
+    regions['dCA1'][f'{i}'] = [[], [], [], []]
+    regions['mPFC'][f'{i}'] = [[], [], [], []]
+    for l in region_list:
+        if np.isnan(all_data.loc[f'{i}_1'][0][f'Trial_LFP_{l}'][0][0]) == False:
+            for j in range(1,11):
+                try:
+                    for k in range(len(all_data.loc[f'{i}_{j}'][0]['Trial_Counter'])):
+                        if all_data.loc[f'{i}_{j}'][0]['Trial_ID'][k][0] == 1:
+                            regions[l][f'{i}'][1].append(np.array(all_data.loc[f'{i}_{j}'][0][f'Trial_LFP_{l}'][k]))
+                        elif all_data.loc[f'{i}_{j}'][0]['Trial_ID'][k][0] == 3:
+                            regions[l][f'{i}'][2].append(np.array(all_data.loc[f'{i}_{j}'][0][f'Trial_LFP_{l}'][k]))
+                        elif all_data.loc[f'{i}_{j}'][0]['Trial_ID'][k][0] == 0:
+                            regions[l][f'{i}'][0].append(np.array(all_data.loc[f'{i}_{j}'][0][f'Trial_LFP_{l}'][k]))
+                        elif all_data.loc[f'{i}_{j}'][0]['Trial_ID'][k][0] == 2:
+                                regions[l][f'{i}'][3].append(np.array(all_data.loc[f'{i}_{j}'][0][f'Trial_LFP_{l}'][k]))
+                        
+    #                        if np.isnan(all_data.loc[f'{i}_{j}'][0]['Trial_FirstLickTime'][k][0]) == False:
+    #                            a = np.min(all_data.loc[f'{i}_{j}'][0]['Trial_LFP_wS1'][k][7000:10000])
+    #                            b = np.where(all_data.loc[f'{i}_{j}'][0]['Trial_LFP_wS1'][k][7000:10000] == a)
+    #                            b = b[0][0]
+    #                            LickTime_data.append([a, b, all_data.loc[f'{i}_{j}'][0]['Trial_FirstLickTime'][k][0], all_data.loc[f'{i}_{j}'][0]['Trial_ID'][k][0]])
+
+                except KeyError:
+                    break
+        else:
+            print(f'no {l} data for mouse {i}')
+
+
+for j in region_list:
+    for i in mice_nbs:        
+        for k in [0,1,2,3]:
+            regions[j][str(i)][k] = np.array(regions[j][str(i)][k])
+    
+        if len(regions[j][str(i)][0]) == 0:
+            regions[j].pop(str(i))
+        else:
+            regions[j][str(i)] = np.array(regions[j][str(i)])
+
+
+
+            
+LickTime_data = np.array(LickTime_data, dtype=object)
+
+for i in range(len(LickTime_data)):
+    if LickTime_data[i, 3]==1:
+        LickTime_data[i,3] = 'green'
+    elif LickTime_data[i, 3]==3:
+        LickTime_data[i,3] = 'blue'
+
+
+stats = {wS1_stats:
 wS1_stats = [[], [], [], []]
 for i in mice_nbs:
     for j in range(4):
-        a = (np.array(wS1[f'{i}'][j]).T)*10**6
+        a = (np.array(regions['wS1'][f'{i}'][j]).T)*10**6
         mean = np.mean(a, axis=1)
         sem = np.std(a, axis=1, ddof=1)/np.sqrt(np.size(a[0]))
         wS1_stats[j].append([mean, sem])
 
 wS1_stats = np.array(wS1_stats)
+
+for i in region_list:
+    for j in mice_nb:
+        if len(regions[region_list][str(i)][0]) == 0:
+            
 
 #%%
 #plot all the trials for a single mice, decide which mouse by determining mice_nb
@@ -106,14 +166,14 @@ wS1_stats = np.array(wS1_stats)
 #total recording length in 0.5 s
 
 
-x1 = np.linspace(-300,200, 10000)
+x = np.linspace(-10,200, 4200)
 mice_nb = 1
 labels = ['Miss', 'Hit', 'FA', 'CR']
 
 fig1 = plt.figure(figsize=(8,4))
 ax1 = plt.subplot(111)
 for i in range(4):
-    ax1.errorbar(x[5800:], wS1_stats[i][mice_nb][0][5800:], wS1_stats[i][mice_nb][1][5800:], label = labels[i], alpha=0.5)
+    ax1.errorbar(x, wS1_stats[i][mice_nb], wS1_stats[i][mice_nb], label = labels[i], alpha=0.5)
 
 ax1.set_ylim(-300, 100)
 ax1.set_ylabel('uV')
@@ -131,7 +191,7 @@ average_across_mice = np.array(average_across_mice)
 fig2 = plt.figure(figsize=(8,4))
 ax1 = plt.subplot(111)
 for i in range(4):
-    ax1.plot(x[5800:], average_across_mice[i][5800:], label = labels[i])
+    ax1.plot(x, average_across_mice[i], label = labels[i])
 ax1.set_ylim(-300, 100)
 ax1.set_ylabel('uV')
 ax1.set_xlabel('Time (ms)')
@@ -140,7 +200,7 @@ ax1.legend()
 #%%
 #plot/compare the average response of neurons between 100-200 ms
 
-data_rs = wS1_stats[0:3, :, 0, 8000:10000].reshape(3, 7*2000)
+data_rs = wS1_stats[0:3, :, 0, 2200:4200].reshape(3, 7*2000)
 
 mean_late_resp = np.mean(data_rs, axis=1)
 late_resp_sd = np.std(data_rs, axis=1, ddof=1)
@@ -162,8 +222,34 @@ tStat_MissFA, pValue_MissFA = stats.ttest_ind(data_all[0], data_all[2], equal_va
 tStat_HitFA, pValue_HitFA = stats.ttest_ind(data_all[1], data_all[2], equal_var = False)
 tStat_MissHit, pValue_MissHit = stats.ttest_ind(data_all[0], data_all[1], equal_var = False)
 
+#%%
+#plot relating the amplitude and the time of the max response
 
+from sklearn.linear_model import LinearRegression
 
+amplitude = LickTime_data[:, 0]*10**6*(-1)
+amplitude = amplitude.reshape(-1,1)
+resp_time = LickTime_data[:, 1]/20+50
+resp_time =resp_time.reshape(-1,1)
+Licktime = LickTime_data[:,2]
 
+model1 = LinearRegression().fit(amplitude, Licktime)
+r_sq = model1.score(amplitude, Licktime)
+
+model2 = LinearRegression().fit(resp_time, Licktime)
+r_sq2 = model2.score(resp_time, Licktime)
+
+x_reg = np.linspace(200, 2000, 11)
+amplitude_pred = model1.coef_*x_reg+model1.intercept_
+RespTime_pred = model2.coef_*x_reg+model2.intercept_
+
+plt.figure(figsize = (10, 5))
+ax1 = plt.subplot(121)
+ax1.scatter(Licktime, amplitude, c=LickTime_data[:, 3], alpha = 0.2) 
+ax1.plot(x_reg, amplitude_pred)
+#ax1.set_ylim(0, 1450)
+
+ax2 = plt.subplot(122)
+ax2.scatter(Licktime, resp_time, c=LickTime_data[:, 3], alpha = 0.2)
 
 
